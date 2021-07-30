9f742c5a313620d8c2488286640412d1a733f186
Raphael De Pieri <raphael.pieri@loreggia.net>
2021-07-29 22:01:23 -0300

    Commit 001

--- init.sh
+++ init.sh
@@ -34,10 +34,11 @@ az aks update \
     --resource-group $RESOURCE_GROUP_NAME \
     --attach-acr $ACR_NAME
 
+
 export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
 
-sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
-sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
+# sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
+# sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
 
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
 echo "-> Resource Group Name: $RESOURCE_GROUP_NAME"
